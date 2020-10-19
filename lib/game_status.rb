182c84e6371ef21b3e3fa83b58e6d041d28e1781
Mike Alessi <mikealessi5@gmail.com>
2020-10-19 15:22:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,11 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-
-
-end
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return win_combination
+    else
+      false
+    end
     end
   end
 
